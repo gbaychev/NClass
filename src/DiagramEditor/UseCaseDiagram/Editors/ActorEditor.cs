7676fa6bc63e614d819c3f0191aa59c533fefe9f
Georgi Baychev <georgi.baychev@gmail.com>
2017-10-01 11:18:11 +0200

    Implement actor editor

---
+++ src/DiagramEditor/UseCaseDiagram/Editors/ActorEditor.cs
@@ -0,0 +1,93 @@
+﻿// NClass - Free class diagram editor
+// Copyright (C) 2006-2009 Balazs Tihanyi
+// Copyright (C) 2016 Georgi Baychev
+// 
+// This program is free software; you can redistribute it and/or modify it under 
+// the terms of the GNU General Public License as published by the Free Software 
+// Foundation; either version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT 
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with 
+// this program; if not, write to the Free Software Foundation, Inc., 
+// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using NClass.DiagramEditor.Diagrams;
+using NClass.DiagramEditor.Diagrams.Editors;
+using NClass.DiagramEditor.UseCaseDiagram.Shapes;
+
+namespace NClass.DiagramEditor.UseCaseDiagram.Editors
+{
+    public partial class ActorEditor :
+#if DEBUG
+    DesignerHelperWindow
+#else
+    EditorWindow
+#endif
+    {
+        private ActorShape shape = null;
+
+        public ActorEditor()
+        {
+            InitializeComponent();
+        }
+
+        internal override void Init(DiagramElement element)
+        {
+            shape = (ActorShape)element;
+
+            txtUseCase.BackColor = Style.CurrentStyle.UseCaseBackColor;
+            txtUseCase.ForeColor = Style.CurrentStyle.UseCaseTextColor;
+            txtUseCase.Text = shape.Name;
+
+            Font font = Style.CurrentStyle.UseCaseFont;
+            txtUseCase.Font = new Font(font.FontFamily, font.SizeInPoints * shape.Diagram.Zoom, font.Style);
+        }
+
+        internal override void Relocate(DiagramElement element)
+        {
+            IDiagram diagram = shape.Diagram;
+
+            if (diagram == null) return;
+
+            using (var g = CreateGraphics())
+            {
+                var graphics = new GdiGraphics(g);
+                Rectangle absolute = shape.GetTextRectangle(graphics, Style.CurrentStyle);
+
+                // The following lines are required because of a .NET bug:
+                // http://connect.microsoft.com/VisualStudio/feedback/ViewFeedback.aspx?FeedbackID=380085
+                if (!MonoHelper.IsRunningOnMono)
+                {
+                    absolute.X -= 3;
+                    absolute.Width += 3;
+                }
+
+                this.SetBounds(
+                    (int)(absolute.X * diagram.Zoom) - diagram.Offset.X + ParentLocation.X,
+                    (int)(absolute.Y * diagram.Zoom) - diagram.Offset.Y + ParentLocation.Y,
+                    (int)(absolute.Width * diagram.Zoom),
+                    (int)(absolute.Height * diagram.Zoom));
+            }
+        }
+
+        public override void ValidateData()
+        {
+            this.shape.Name = this.txtUseCase.Text;
+        }
+
+        private void txtComment_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && e.Modifiers != Keys.None ||
+                e.KeyCode == Keys.Escape)
+            {
+                shape.HideEditor();
+            }
+        }
+    }
+}
--- src/DiagramEditor/UseCaseDiagram/Shapes/ActorShape.cs
+++ src/DiagramEditor/UseCaseDiagram/Shapes/ActorShape.cs
@@ -19,23 +19,32 @@ using System.Drawing;
 using NClass.Core;
 using NClass.DiagramEditor.Diagrams;
 using NClass.DiagramEditor.Diagrams.Shapes;
+using NClass.DiagramEditor.UseCaseDiagram.Editors;
 
 namespace NClass.DiagramEditor.UseCaseDiagram.Shapes
 {
     public class ActorShape : Shape
     {
+        private bool isEditorShown = false;
+        private ActorEditor editor;
         private Actor actor;
         private const int DefaultWidth = 75;
         private const int DefaultHeight = 150;
         private const int PaddingSize = 5;
         private readonly Size defaultSize = new Size(DefaultWidth, DefaultHeight);
         private const int Proportion = 2; // Height / Width
+        private readonly StringFormat stringFormat;
 
 
         public ActorShape(Actor entity) : base(entity)
         {
             actor = entity;
             this.MinimumSize = defaultSize;
+            editor = new ActorEditor();
+            this.stringFormat = StringFormat.GenericTypographic;
+            this.stringFormat.Alignment = StringAlignment.Center;
+            this.stringFormat.Trimming = StringTrimming.EllipsisWord;
+            this.stringFormat.FormatFlags = StringFormatFlags.LineLimit;
         }
 
         public override void Draw(IGraphics g, bool onScreen, Style style)
@@ -46,9 +55,6 @@ namespace NClass.DiagramEditor.UseCaseDiagram.Shapes
 
         private void DrawText(IGraphics graphics, bool onScreen, Style style)
         {
-            var stringFormat = new StringFormat();
-            stringFormat.Alignment = StringAlignment.Center;
-            stringFormat.Trimming = StringTrimming.EllipsisCharacter;
             //graphics.DrawRectangle(new Pen(Color.Black), new Rectangle(this.Left, this.Top, this.Width, this.Height) );
             //graphics.DrawRectangle(new Pen(Color.Black), GetTextRectangle(graphics, style) );
             graphics.DrawString(
@@ -56,7 +62,7 @@ namespace NClass.DiagramEditor.UseCaseDiagram.Shapes
                                 style.StaticMemberFont,
                                 new SolidBrush(style.ActorTextColor),
                                 GetTextRectangle(graphics, style),
-                                stringFormat);
+                                this.stringFormat);
         }
 
         private void DrawSurface(IGraphics graphics, bool onScreen, Style style)
@@ -101,6 +107,12 @@ namespace NClass.DiagramEditor.UseCaseDiagram.Shapes
             {
                 this.size.Width = this.Height / Proportion;
             }
+            if (isEditorShown)
+            {
+                editor.Relocate(this);
+                if (!editor.Focused)
+                    editor.Focus();
+            }
             base.OnResize(e);
         }
 
@@ -125,7 +137,7 @@ namespace NClass.DiagramEditor.UseCaseDiagram.Shapes
             return Width;
         }
 
-        private Rectangle GetTextRectangle(IGraphics g, Style style)
+        public Rectangle GetTextRectangle(IGraphics g, Style style)
         {
             SizeF textSize = g.MeasureString(this.Entity.Name, style.ActorFont);
             int left = this.Left + PaddingSize;
@@ -144,9 +156,53 @@ namespace NClass.DiagramEditor.UseCaseDiagram.Shapes
             return useCaseDiagram.InsertActor(actor.Clone());
         }
 
+        public string Name
+        {
+            get { return actor.Name; }
+            set
+            {
+                if (String.Compare(actor.Name, value, StringComparison.Ordinal) != 0)
+                {
+                    actor.Name = value;
+                }
+            }
+        }
+
         public static Rectangle GetOutline(Style style)
         {
             return new Rectangle(0, 0, DefaultWidth, DefaultHeight);
+        }
+        protected override void OnDoubleClick(AbsoluteMouseEventArgs e)
+        {
+            ShowEditor();
+            base.OnDoubleClick(e);
+        }
+
+        protected internal override void ShowEditor()
+        {
+            if (!isEditorShown)
+            {
+                editor.Init(this);
+                editor.Relocate(this);
+                ShowWindow(editor);
+                editor.Focus();
+                isEditorShown = true;
+            }
+        }
+
+        protected internal override void HideEditor()
+        {
+            if (isEditorShown)
+            {
+                HideWindow(editor);
+                isEditorShown = false;
+            }
+        }
+
+        protected override void OnMove(MoveEventArgs e)
+        {
+            HideEditor();
+            base.OnMove(e);
         }
     }
 }
