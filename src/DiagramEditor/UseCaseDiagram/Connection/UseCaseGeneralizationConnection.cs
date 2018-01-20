6df857cea5e61107908be54d5cea90e11e07a588
Georgi Baychev <georgi.baychev@gmail.com>
2018-01-20 15:25:35 +0100

    Add end cap to use case generalization

--- src/DiagramEditor/UseCaseDiagram/Connection/UseCaseGeneralizationConnection.cs
+++ src/DiagramEditor/UseCaseDiagram/Connection/UseCaseGeneralizationConnection.cs
@@ -13,7 +13,9 @@
 // this program; if not, write to the Free Software Foundation, Inc., 
 // 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+using System.Drawing;
 using NClass.Core;
+using NClass.DiagramEditor.ClassDiagram.Connections;
 using NClass.DiagramEditor.Diagrams;
 using NClass.DiagramEditor.Diagrams.Shapes;
 
@@ -22,6 +24,7 @@ namespace NClass.DiagramEditor.UseCaseDiagram.Connection
     public class UseCaseGeneralizationConnection : Diagrams.Connections.Connection
     {
         private UseCaseGeneralization generalization;
+        private Pen linePen = new Pen(Color.Black);
 
         public UseCaseGeneralizationConnection(Relationship relationship, Shape firstShape, Shape secondShape)
             : base(relationship, firstShape, secondShape)
@@ -45,6 +48,15 @@ namespace NClass.DiagramEditor.UseCaseDiagram.Connection
             {
                 return false;
             }
+        }
+
+        protected override void DrawEndCap(IGraphics g, bool onScreen, Style style)
+        {
+            this.linePen.Color = style.RelationshipColor;
+            this.linePen.Width = style.RelationshipWidth;
+
+            g.FillPath(Brushes.White, Arrowhead.ClosedArrowPath);
+            g.DrawPath(linePen, Arrowhead.ClosedArrowPath);
         }
     }
 }
