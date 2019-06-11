6c06679abf21013c89a289efca9e4caab487077f
Georgi Baychev <georgi.baychev@gmail.com>
2019-06-11 14:11:34 +0200

    Add unit test for copy/paste packages

---
+++ src/Tests/CopyPasteTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Threading;
+using NClass.Core;
+using NClass.DiagramEditor.ClassDiagram;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Tests
+{
+    [TestFixture]
+    public class CopyPasteTests
+    {
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void CopyPastePackageWorks()
+        {
+            var diagram = new ClassDiagram(Language.GetLanguage("csharp"));
+            var other = new ClassDiagram(Language.GetLanguage("csharp"));
+
+            diagram.CreateShapeAt(EntityType.Package, new Point(0, 0));
+            diagram.CreateShapeAt(EntityType.Class, new Point(1, 1));
+
+            diagram.SelectAll();
+            diagram.Copy();
+            other.Paste();
+
+            other.Model.Entities.Count().ShouldBe(2);
+            other.ShapeCount.ShouldBe(2);
+        }
+    }
+}
