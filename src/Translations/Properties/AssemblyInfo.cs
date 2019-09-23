2dd62b810295146ccf1886de4ad3d24cad4f33dc
Georgi Baychev <georgi.baychev@gmail.com>
2019-09-23 19:11:52 +0200

    Assembly version bump

--- src/Translations/Properties/AssemblyInfo.cs
+++ src/Translations/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
